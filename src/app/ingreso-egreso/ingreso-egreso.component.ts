020e9cdea7a972972c74b733b9d0ff32bba6c017
Patricio Albizu <albizupatricio@github.com>
2019-05-04 01:48:17 -0300

    First commit - templeate ready

---
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-egreso',
+  templateUrl: './ingreso-egreso.component.html',
+  styles: []
+})
+export class IngresoEgresoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
